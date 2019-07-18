b59884c1d3c49b529f201007f341bac22ed48f7f
Billie Tran <0billtran@gmail.com>
2019-07-18 02:54:15 -0700

    Basic Structure for App.js. Included several imports of components: Toolbar, Footer, Background, Store, LaptopForm. Changed css import to module.css and Dependiences for FontAwesome usage.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import Toolbar from './component/Header/Toolbar/Toolbar';
+import Footer from './component/Footer/Footer';
+import Background from './component/BackgroundContainer/Background';
+import Store from './component/Store/Store';
+import LaptopForm from './container/LaptopForm/LaptopForm';
+
+import styles from './App.module.css';
+
+import {library} from '@fortawesome/fontawesome-svg-core';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faShoppingCart, faSearch, faEnvelope} from '@fortawesome/free-solid-svg-icons';
+
+library.add(faShoppingCart, faSearch, faEnvelope);
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.Container}>
+      <Toolbar/>
+        <LaptopForm/>
+        <Background/>
+        <Store/>
+      <Footer/>
     </div>
   );
 }
