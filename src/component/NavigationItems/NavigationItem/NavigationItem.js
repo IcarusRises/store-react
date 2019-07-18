889ac412bb9088eee3b2e2ec0581c4e09e819f8e
Billie Tran <0billtran@gmail.com>
2019-07-18 03:36:18 -0700

    Simple link item and style

---
+++ src/component/NavigationItems/NavigationItem/NavigationItem.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+import styles from './NavigationItem.module.css';
+
+const navigationItem = (props) => (
+    <li className={styles.Items}>
+        <Link
+            to={props.link}
+            >{props.children}</Link>
+    </li>
+)
+
+export default navigationItem;
