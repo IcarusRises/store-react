5043d46578f495707a66dee8674d54d0fb0a4815
Billie Tran <0billtran@gmail.com>
2019-07-31 02:27:44 -0700

    Testing Routing and added Buttons and icons

--- src/component/Header/NavigationItems/NavigationItems.js
+++ src/component/Header/NavigationItems/NavigationItems.js
@@ -1,15 +1,27 @@
 import React from 'react';
 
 import NavigationItem from './NavigationItem/NavigationItem';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+
+import CreateLaptop from '../../../container/Form/CreateLaptop';
+
+
 
 import styles from './NavigationItems.module.css';
 
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 
 const navigationItems = () => (
-    <ul className={styles.NavigationItems}>
-        <NavigationItem link='/cart'><FontAwesomeIcon icon="shopping-cart"/></NavigationItem>
-    </ul>
+    <Router>
+        <ul className={styles.NavigationItems}>
+            <NavigationItem link='/create'><FontAwesomeIcon icon="plus" /></NavigationItem>
+            <NavigationItem link='/'><FontAwesomeIcon icon="minus" /></NavigationItem>
+            <NavigationItem link='/cart'><FontAwesomeIcon icon="shopping-cart" /></NavigationItem>
+        </ul>
+
+        <Route path="/create" component={CreateLaptop}/> 
+    </Router>
+    
 )
 
 export default navigationItems;
