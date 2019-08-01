02930adb9b33d8c2b42e1fbde50f0c6548eb52bf
Billie Tran <0billtran@gmail.com>
2019-08-01 03:21:59 -0700

    Added background to store

--- src/component/Store/Store.js
+++ src/component/Store/Store.js
@@ -1,12 +1,17 @@
 import React from 'react';
+
+import Background from '../BackgroundContainer/Background';
 import CardGenerator  from '../../container/CardGenerator/CardGenerator';
 
 import styles from './Store.module.css';
 
 const store = () => (
-    <div className={styles.Center}>
-        <div className={styles.Store}>
-            <CardGenerator/>
+    <div>
+        <Background/>
+        <div className={styles.Center}>
+            <div className={styles.Store}>
+                <CardGenerator/>
+            </div>
         </div>
     </div>
 )
