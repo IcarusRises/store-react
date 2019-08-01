39824adb4e0851f74f9c38f3ea2a878dc926728f
Billie Tran <0billtran@gmail.com>
2019-08-01 06:35:36 -0700

    Added more Fields for basic form layout

--- src/container/Form/LaptopForm.js
+++ src/container/Form/LaptopForm.js
@@ -1,20 +1,62 @@
-import React from 'react'; 
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-
-import EditLaptop from './EditLaptop';
-import CreateLaptop from './CreateLaptop';
-
+import React, {Component} from 'react';
+import axios from 'axios';
 import styles from './LaptopForm.module.css';
 
+class LaptopForm extends Component{
+    state={
+        laptop: {
+            brand: "",
+        }    
+    }
 
+    inputChangeHandler = (e) => {
+        const laptop = e.target.value
+        console.log(laptop)
+    };
 
-function LaptopForm(){
-    return(
-        <Switch>
-            <Route path="/update/:id" exact component={EditLaptop}/>
-            <Route path="/create" component={CreateLaptop}/>
-        </Switch>
-    )
+    onSubmit = (e) => {
+        e.preventDefault();
+
+        const laptop = {
+            brand: this.state.laptop.brand
+        }
+        
+        axios.post('http://localhost:3001/laptops/add', laptop)
+            .then(res => console.log(res.data))
+
+        this.setState({
+            laptop: laptop
+        })
+    }
+
+    render(){
+        return(
+            <div className={styles.Container}>
+                <p className={styles.Title}>New Laptop</p>
+                <form action="" className={styles.Form} onSubmit={this.onSubmit}>
+                    <input type="text" name="" value="" placeholder="Brand"
+                    onChange={this.inputChangeHandler}/>
+                    <input type="text" name="" value="" placeholder="Name"
+                    onChange={this.inputChangeHandler}/>
+                    <input type="text" name="" value="" placeholder="Main Image"
+                    onChange={this.inputChangeHandler}/>
+                    <input type="text" name="" value="" placeholder="Image"
+                    onChange={this.inputChangeHandler}/>
+                    <input type="text" name="" value="" placeholder="Image"
+                    onChange={this.inputChangeHandler}/>
+                    <input type="text" name="" value="" placeholder="Original Price"
+                    onChange={this.inputChangeHandler}/>
+                    <input type="text" name="" value="" placeholder="Savings"
+                    onChange={this.inputChangeHandler}/>
+                    <input type="text" name="" value="" placeholder="Price"
+                    onChange={this.inputChangeHandler}/>
+                    <button>
+                        Submit
+                    </button>
+                </form>
+            </div>
+        )
+    }
 }
 
 export default LaptopForm;
