eae2aae56515c57ca251617d3fedd9c025b0641e
Billie Tran <0billtran@gmail.com>
2019-08-01 03:30:17 -0700

    Removed react-router-dom from import, removed Router from navigationItems

--- src/component/Header/NavigationItems/NavigationItems.js
+++ src/component/Header/NavigationItems/NavigationItems.js
@@ -1,26 +1,17 @@
 import React from 'react';
 
 import NavigationItem from './NavigationItem/NavigationItem';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
-
-import CreateLaptop from '../../../container/Form/CreateLaptop';
-
-
 
 import styles from './NavigationItems.module.css';
 
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 
 const navigationItems = () => (
-    <Router>
         <ul className={styles.NavigationItems}>
-            <NavigationItem link='/create'><FontAwesomeIcon icon="plus" /></NavigationItem>
+            <NavigationItem link='/add'><FontAwesomeIcon icon="plus" /></NavigationItem>
             <NavigationItem link='/'><FontAwesomeIcon icon="minus" /></NavigationItem>
             <NavigationItem link='/cart'><FontAwesomeIcon icon="shopping-cart" /></NavigationItem>
         </ul>
-
-        <Route path="/create" component={CreateLaptop}/> 
-    </Router>
     
 )
 
