66ba5f0f5d06361fefa7c71f44021420cb04f047
Billie Tran <0billtran@gmail.com>
2019-08-08 02:45:28 -0700

    Added props

--- src/component/Carousel/Carousel.js
+++ src/component/Carousel/Carousel.js
@@ -5,19 +5,19 @@ import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import styles from './Carousel.module.css';
 
-const carousel = () => (
+const carousel = (props) => (
     <div className={styles.Container}>
-        <Carousel showThumbs={false} useKeyboardArrows>
+        <Carousel showThumbs={props.boolean} useKeyboardArrows>
             <div>
-                <img src='https://smartslider3.com/wp-content/uploads/2017/12/slidersucksogimage.png' alt='1'/>
+                <img src={props.mainImage}alt='1'/>
                 <p className="legend">Legend 1</p>
             </div>
             <div>
-                <img src="https://smartslider3.com/wp-content/uploads/2017/12/slidersucksogimage.png" alt='1'/>
+                <img src={props.secondImage} alt='1'/>
                 <p className="legend">Legend 2</p>
             </div>
             <div>
-                <img src="https://smartslider3.com/wp-content/uploads/2017/12/slidersucksogimage.png" alt='1'/>
+                <img src={props.thirdImage} alt='1'/>
                 <p className="legend">Legend 3</p>
             </div>
         </Carousel>
