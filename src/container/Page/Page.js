4272292f30d117a606443e28d47117651744abf7
Billie Tran <0billtran@gmail.com>
2019-08-07 23:43:51 -0700

    Included redirect for react-router-dom, imported Laptop and Contact, added moreroutes

--- src/container/Page/Page.js
+++ src/container/Page/Page.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react';
-import {Switch, Route} from 'react-router-dom';
+import {Switch, Route, Redirect} from 'react-router-dom';
 
 import Toolbar from '../../component/Header/Toolbar/Toolbar';
 import LaptopForm from '../Form/LaptopForm';
 import Store from '../../component/Store/Store';
+import Laptop from '../../component/Laptop/Laptop';
+import Contact from '../Contact/Contact';
 import Footer from '../../component/Footer/Footer';
 
 class Page extends Component{
@@ -12,8 +14,11 @@ class Page extends Component{
             <div>
                 <Toolbar/>
                 <Switch>
-                    <Route path="/" exact component={Store}/>
+                    <Route exact path="/" exact component={Store}/>
                     <Route path="/add" component={LaptopForm}/>
+                    <Route path="/contact" component={Contact}/>
+                    <Route path="/laptops/:id" component={Laptop}/>
+                    <Redirect to="/"/>
                 </Switch>
                 <Footer/>
             </div>
