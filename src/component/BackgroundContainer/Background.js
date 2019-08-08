6c2894413b1bafa304596f2d2441063960eea8c7
Billie Tran <0billtran@gmail.com>
2019-08-08 02:58:42 -0700

    Using Props

--- src/component/BackgroundContainer/Background.js
+++ src/component/BackgroundContainer/Background.js
@@ -7,7 +7,11 @@ import styles from './Background.module.css';
 const background = () => (
     <div className={styles.Background}>
         <Sidebar/>
-        <Carousel/>
+        <Carousel 
+            boolean={false} 
+            mainImage={'https://dt2sdf0db8zob.cloudfront.net/wp-content/uploads/2018/09/image2-27-840x430.png'}
+            secondImage={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtSoDWsQasjT6229m_l-fkW72AjIWImYFfr7wK73TLa6wmKMBC'}
+            thirdImage={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtSoDWsQasjT6229m_l-fkW72AjIWImYFfr7wK73TLa6wmKMBC'}/>
     </div>
 )
 
