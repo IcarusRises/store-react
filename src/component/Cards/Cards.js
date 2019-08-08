698ae7c28d3bb18195c8e2988255998c18b6bf89
Billie Tran <0billtran@gmail.com>
2019-08-07 23:37:56 -0700

    Added more props to cards

--- src/component/Cards/Cards.js
+++ src/component/Cards/Cards.js
@@ -1,19 +1,36 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import styles from './Cards.module.css'
+
 
 const cards = (props) => (
     <div className={styles.Card}>
-        <div className={styles.Image}>
-            <img className={styles.CardImage} src={props.image} alt="test image"/>
-        </div>
-        <div>
-            <p className={styles.Header}>
-                {props.name}
-            </p>
-            <p className={styles.Price}>
-                {props.price}
-            </p>
-        </div>
+        <Link to={"/laptops/" + props.id} className={styles.Card}>
+            <div className={styles.Image}>
+                <img className={styles.CardImage} src={props.image} alt="test image"/>
+            </div>
+            <div>
+                <p className={styles.Header}>
+                    {props.brand}
+                </p>
+                <p>
+                    {props.name}
+                </p>
+                <p>
+                    {props.original}
+                </p>
+                <p>
+                    {props.savings}
+                </p>
+                <p>
+                    {props.price}
+                </p>
+                
+                <p className={styles.Price}>
+                    {props.price}
+                </p>
+            </div>
+        </Link>        
         <button className={styles.Cart}>
                 Add to Cart
         </button>
