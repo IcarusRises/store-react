c785470b5077f068f2386ad4bcb5f4c7f41706fb
Billie Tran <0billtran@gmail.com>
2019-08-09 06:37:28 -0700

    Added a purchase box

--- src/component/Laptop/Laptop.js
+++ src/component/Laptop/Laptop.js
@@ -1,6 +1,11 @@
 import React, {Component} from 'react';
 import Axios from 'axios';
 
+import Carousel from '../Carousel/Carousel';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+
+import styles from './Laptop.module.css';
+
 class Laptop extends Component{
 
     state = {
@@ -21,14 +26,57 @@ class Laptop extends Component{
     render(){
         console.log(this.state.laptop)
         return(
-            <div>
-                <p>
-                    {this.state.laptop.brand}
-                </p>
-                <p>
-                    {this.state.laptop.name}
-                </p>
-                <img src={this.state.laptop.image} alt=""/>
+            <div className={styles.container}>
+                <div className={styles.row}>
+                    <div className={styles.column}>
+                        <div className={styles.carousel}>
+                            <Carousel
+                                boolean={true}
+                                width='500px'
+                                mainImage={this.state.laptop.image}
+                                secondImage={this.state.laptop.image}
+                                thirdImage={this.state.laptop.image}/>
+                        </div>
+                    </div>
+                    <div className={`${styles.column} ${styles.laptop}`}>
+                        <p>
+                            {this.state.laptop.brand}
+                        </p>
+                        <p>
+                            {this.state.laptop.name}
+                        </p>
+                    </div>
+                    <div className={`${styles.column} ${styles.box}`}>
+                        <p className={styles.box_sold}>Sold and Shipped By: <span>Linh Cell</span></p>
+                        <ul className={styles.box_list}>
+                            <li className={styles.box_price}>
+                                ${this.state.laptop.price}
+                            </li>
+                            <li className={styles.box_savings}>
+                                Save {this.state.laptop.savings}
+                            </li>
+                            <li className={styles.box_original}>
+                                original <span>${this.state.laptop.original}</span>
+                            </li>
+                        </ul>
+                        <div className={styles.box_buttons_container}>
+                            <div>
+                                <button className={`${styles.button_pointer} ${styles.box_buttons_inline} ${styles.number_button_subtract}`}>
+                                    <FontAwesomeIcon icon="minus" />
+                                </button>
+                                <div className={styles.box_buttons_inline}>
+                                    <input type="number" className={styles.number}/>
+                                </div>
+                                <button className={`${styles.button_pointer} ${styles.box_buttons_inline} ${styles.number_button_add}`}>
+                                    <FontAwesomeIcon icon="plus" />
+                                </button>
+                                <button className={`${styles.button_pointer} ${styles.button_cart} ${styles.box_buttons_inline}`}>
+                                    <FontAwesomeIcon icon="shopping-cart"/> Add to Cart
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     };
