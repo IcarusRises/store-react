a496eef35a28f74cd6040d3ec37504349448395f
Billie Tran <0billtran@gmail.com>
2019-08-20 06:54:34 -0700

    Creation of breadcrumbs component

--- src/component/BreadCrumbs/BreadCrumbs.js
+++ src/component/BreadCrumbs/BreadCrumbs.js
@@ -1,11 +1,14 @@
 import React from 'react'
-
 import {Link} from 'react-router-dom';
 
-const breadCrumbs = (props) => {
-    return props.links.map(({to, label}) => 
-        <li><Link to={to}>{label}</Link></li>    
-    )
-}
+import styles from './BreadCrumbs.module.css';
+
+const breadCrumbs = (props) => (
+    <ul className={styles.breadCrumbs}>
+        {props.links.map(({to, label}) => 
+            <li><Link to={to}>{label}</Link></li>    
+        )}
+    </ul>
+)
 
 export default breadCrumbs
