f6d6a4658d02c82584bca72adb13b029b7180631
Billie Tran <0billtran@gmail.com>
2019-08-27 03:55:09 -0700

    Added Props and cleaned code

--- src/component/Carousel/Carousel.js
+++ src/component/Carousel/Carousel.js
@@ -3,22 +3,18 @@ import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
 
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import styles from './Carousel.module.css';
 
 const carousel = (props) => (
     <div>
-        <Carousel showThumbs={props.boolean} useKeyboardArrows>
+        <Carousel showThumbs={props.boolean} width={props.width} showStatus={false} showArrows={false} useKeyboardArrows>
             <div>
                 <img src={props.mainImage}alt='1'/>
-                <p className="legend">Legend 1</p>
             </div>
             <div>
                 <img src={props.secondImage} alt='1'/>
-                <p className="legend">Legend 2</p>
             </div>
             <div>
                 <img src={props.thirdImage} alt='1'/>
-                <p className="legend">Legend 3</p>
             </div>
         </Carousel>
     </div>
