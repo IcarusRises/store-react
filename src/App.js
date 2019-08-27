2be20f517433c1ddbb3fd5ab7889d460c52b30f2
Billie Tran <0billtran@gmail.com>
2019-08-27 03:29:41 -0700

    Cleaned up the code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import React from 'react';
-import {BrowserRouter} from 'react-router-dom';
-
-import Page from './container/Page/Page';
-
-import styles from './App.module.css';
-
+import React, {Component} from 'react';
 import {library} from '@fortawesome/fontawesome-svg-core';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faShoppingCart, faSearch, faEnvelope, faPlus, faMinus} from '@fortawesome/free-solid-svg-icons';
+import Page from './container/Page/Page';
+import styles from './App.module.css';
 
 library.add(faShoppingCart, faSearch, faEnvelope, faPlus, faMinus);
 
-function App() {
-
-  return (
-    <BrowserRouter>
-      <div className={styles.Container}>
-        <Page/>
-      </div>
-    </BrowserRouter>
-  );
+class App extends Component{
+  render(){
+    return(
+        <div className={styles.Container}>
+          <Page/>
+        </div>
+    )
+  }
 }
 
 export default App;
