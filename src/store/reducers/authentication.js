ff5c96a6f2efe096165d7ad50385aa864aebdbce
Billie Tran <0billtran@gmail.com>
2019-09-04 23:35:20 -0700

    Added sign out

--- src/store/reducers/authentication.js
+++ src/store/reducers/authentication.js
@@ -5,6 +5,9 @@ const authenticated = (state = initialState, action) => {
     switch(action.type){
         case actionTypes.FETCH_USER: 
             return action.user || null;
+        case actionTypes.SIGN_OUT_SUCCESS:
+            console.log('Sign Out Successful');
+            return state;
         default: 
             return state;    
     }
