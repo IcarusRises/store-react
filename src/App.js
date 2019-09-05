24102d8787d9e5672d66275f48524468b4fe7182
Billie Tran <0billtran@gmail.com>
2019-09-05 02:48:55 -0700

    Updated destination of Page component to match change

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {library} from '@fortawesome/fontawesome-svg-core';
 import {faShoppingCart, faSearch, faEnvelope, faPlus, faMinus} from '@fortawesome/free-solid-svg-icons';
-import Page from './container/Page/Page';
+import Page from './component/Page/Page';
 import styles from './App.module.css';
 
 library.add(faShoppingCart, faSearch, faEnvelope, faPlus, faMinus);
