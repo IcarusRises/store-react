d8503cba196c2d8dd502cd1f7c44eb0a860ba7c2
Billie Tran <0billtran@gmail.com>
2019-09-11 02:22:10 -0700

    Added redux state for authentication as well a navbar for authorized users

--- src/component/Page/Page.js
+++ src/component/Page/Page.js
@@ -1,23 +1,30 @@
 import React from 'react';
 import {Switch, Route, Redirect} from 'react-router-dom';
-import Toolbar from '../../component/Header/Toolbar/Toolbar';
-import LaptopForm from '../Form/LaptopForm';
-import Store from '../../component/Store/Store';
+import {connect} from 'react-redux';
+import AuthorizedNavBar from '../AuthorizedNavBar/AuthorizedNavBar';
+import Toolbar from '../Header/Toolbar/Toolbar';
+import LaptopForm from '../../container/Form/LaptopForm';
+import Store from '../Store/Store';
 import Laptop from '../../container/Laptop/Laptop';
-import Contact from '../Contact/Contact';
-import SignIn from '../../component/SignIn/SignIn';
+import EditLaptop from '../../container/Form/EditLaptop';
+import Contact from '../../container/Contact/Contact';
+import SignIn from '../SignIn/SignIn';
 import SearchResults from '../../container/SearchResults/SearchResults';
-import Footer from '../../component/Footer/Footer';
+import Footer from '../Footer/Footer';
 
-const page = () => {
+const page = (props) => {
+    const {authenticated} = props;
+    const authVerified = authenticated.uid ? <AuthorizedNavBar/> : '';
     return(
         <div>
             <Toolbar/>
+            {authVerified}
             <Switch>
                 <Route exact path="/" component={Store}/>
                 <Route path="/add" component={LaptopForm}/>
                 <Route path="/contact" component={Contact}/>
-                <Route path="/laptops/:id" component={Laptop}/>
+                <Route path="/laptops/:id" exact component={Laptop}/>
+                <Route path='/laptops/edit/:id' component={EditLaptop}/>
                 <Route path='/signin' component={SignIn}/>
                 <Route path='/search' render={(props) => 
                     <SearchResults {...props} />
@@ -30,4 +37,10 @@ const page = () => {
 }
 
 
-export default page;
+const mapStateToProps = (state) => {
+    return{
+        authenticated: state.authenticated
+    }
+};
+
+export default connect(mapStateToProps)(page);
