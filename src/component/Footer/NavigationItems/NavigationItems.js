d7ff624c388518c4ef18834bd049c0103fa12d09
Billie Tran <0billtran@gmail.com>
2019-09-17 02:50:03 -0700

    Added more links

--- src/component/Footer/NavigationItems/NavigationItems.js
+++ src/component/Footer/NavigationItems/NavigationItems.js
@@ -1,12 +1,14 @@
 import React from 'react';
-
 import NavigationItem from '../../NavigationItems/NavigationItem/NavigationItem';
+import styles from './NavigationItems.module.css';
 
 const navigationItems = (props) => (
-    <ul>
+    <ul className={styles.NavigationItems_List}>
         <li>{props.name}</li>
-        <NavigationItem link='/'>Home</NavigationItem>
+        <NavigationItem link='#'>About Us</NavigationItem>
         <NavigationItem link='/contact'>Contact Us</NavigationItem>
+        <NavigationItem link='#'>Privacy Policy</NavigationItem>
+        <NavigationItem link='#'>Terms and Conditions</NavigationItem>
     </ul>
 )
 
