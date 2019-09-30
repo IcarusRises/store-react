79362554b6699d72f9d0b50b2f28f21037eb7ff6
Billie Tran <0billtran@gmail.com>
2019-09-30 02:33:57 -0700

    Added container styling

--- src/component/Carousel/Carousel.js
+++ src/component/Carousel/Carousel.js
@@ -1,11 +1,10 @@
 import React from 'react';
-
 import { Carousel } from 'react-responsive-carousel';
-
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import styles from './Carousel.module.css';
 
 const carousel = (props) => (
-    <div>
+    <div className={styles.Carousel_Container}>
         <Carousel showThumbs={props.boolean} width={props.width} showStatus={false} showArrows={false} useKeyboardArrows>
             <div>
                 <img src={props.mainImage}alt='1'/>
