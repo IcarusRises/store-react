f8b387280f7729546d53ad4c4402d93f86f75d4e
Billie Tran <0billtran@gmail.com>
2019-09-30 02:59:15 -0700

    Changed divs to Fragments, added grid areas and images

--- src/component/Store/Store.js
+++ src/component/Store/Store.js
@@ -1,19 +1,30 @@
-import React from 'react';
-
-import Background from '../BackgroundContainer/Background';
+import React, {Fragment} from 'react';
+import Sidebar from '../Sidebar/Sidebar';
 import CardGenerator  from '../../container/CardGenerator/CardGenerator';
+import Carousel from '../Carousel/Carousel';
 
 import styles from './Store.module.css';
 
 const store = () => (
-    <div>
-        <Background/>
-        <div className={styles.Center}>
+    <Fragment>
+        <div className={styles.Store_Empty_Area_E}>
+        </div>
+        <Sidebar/>
+        <Carousel
+            boolean={false} 
+            mainImage={'https://dt2sdf0db8zob.cloudfront.net/wp-content/uploads/2018/09/image2-27-840x430.png'}
+            secondImage={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtSoDWsQasjT6229m_l-fkW72AjIWImYFfr7wK73TLa6wmKMBC'}
+            thirdImage={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtSoDWsQasjT6229m_l-fkW72AjIWImYFfr7wK73TLa6wmKMBC'}
+        />
+        <div className>
+
+        </div>
+        <div className={styles.Store_Container}>
             <div className={styles.Store}>
                 <CardGenerator/>
             </div>
         </div>
-    </div>
+    </Fragment>
 )
 
 
