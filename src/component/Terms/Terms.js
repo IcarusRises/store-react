17147bcd1f6845940cd24aa239be50d6c8b9ec85
Billie Tran <0billtran@gmail.com>
2019-10-06 07:18:47 -0700

    Added a new page for terms section

---
+++ src/component/Terms/Terms.js
@@ -0,0 +1,77 @@
+import React, {Fragment} from 'react';
+import BreadCrumbs from '../BreadCrumbs/BreadCrumbs';
+import styles from './Terms.module.css';
+
+
+const terms = () => (
+    <Fragment>
+        <BreadCrumbs links={[
+            {to: '/', label: 'Home'},
+            {to:'/terms-conditions', label: 'Terms and Conditions'},
+        ]}/>
+        <div className={`${styles.Terms_Area_A}`}>
+            A
+        </div>
+        <div className={`${styles.Terms_Area_B}`}>
+            <p className={ `${styles.Terms_Title}`}>
+                Terms and Conditions
+            </p>
+            <p className={`${styles.Terms_Topic_Title}`}>
+                Example    
+            </p>
+            <p className={`${styles.Terms_Topic_Blurb}`}>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam saepe quibusdam facilis? Accusamus excepturi, voluptates, placeat magnam quod voluptas esse porro ex vitae repellat at, doloribus blanditiis iste ab. Doloremque?
+                Pariatur, nisi omnis facere, officia porro voluptatibus magnam placeat exercitationem consequuntur repudiandae dolorem dolor illum, quibusdam asperiores commodi. Eaque iure est praesentium adipisci suscipit, laboriosam dolor et excepturi nulla modi.
+                Dolores, rem vitae voluptas voluptatum voluptatibus saepe sed fuga eius ab asperiores, temporibus, tempore minima eligendi dignissimos dicta. Porro, consequatur eligendi. Provident deleniti officia expedita ducimus aliquid omnis sed accusantium!
+                Numquam, possimus qui officiis corrupti nobis ad iure illo voluptates, impedit placeat repudiandae culpa cumque! Tempore quis adipisci doloremque expedita. Aspernatur maiores dolorum ipsam neque. Obcaecati tempore et velit modi.
+            </p>
+            <p className={`${styles.Terms_Topic_Title}`}>
+                Example    
+            </p>
+            <p className={`${styles.Terms_Topic_Blurb}`}>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam saepe quibusdam facilis? Accusamus excepturi, voluptates, placeat magnam quod voluptas esse porro ex vitae repellat at, doloribus blanditiis iste ab. Doloremque?
+                Pariatur, nisi omnis facere, officia porro voluptatibus magnam placeat exercitationem consequuntur repudiandae dolorem dolor illum, quibusdam asperiores commodi. Eaque iure est praesentium adipisci suscipit, laboriosam dolor et excepturi nulla modi.
+                Dolores, rem vitae voluptas voluptatum voluptatibus saepe sed fuga eius ab asperiores, temporibus, tempore minima eligendi dignissimos dicta. Porro, consequatur eligendi. Provident deleniti officia expedita ducimus aliquid omnis sed accusantium!
+                Numquam, possimus qui officiis corrupti nobis ad iure illo voluptates, impedit placeat repudiandae culpa cumque! Tempore quis adipisci doloremque expedita. Aspernatur maiores dolorum ipsam neque. Obcaecati tempore et velit modi.
+            </p>
+            <p className={`${styles.Terms_Topic_Title}`}>
+                Example    
+            </p>
+            <p className={`${styles.Terms_Topic_Blurb}`}>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam saepe quibusdam facilis? Accusamus excepturi, voluptates, placeat magnam quod voluptas esse porro ex vitae repellat at, doloribus blanditiis iste ab. Doloremque?
+                Pariatur, nisi omnis facere, officia porro voluptatibus magnam placeat exercitationem consequuntur repudiandae dolorem dolor illum, quibusdam asperiores commodi. Eaque iure est praesentium adipisci suscipit, laboriosam dolor et excepturi nulla modi.
+                Dolores, rem vitae voluptas voluptatum voluptatibus saepe sed fuga eius ab asperiores, temporibus, tempore minima eligendi dignissimos dicta. Porro, consequatur eligendi. Provident deleniti officia expedita ducimus aliquid omnis sed accusantium!
+                Numquam, possimus qui officiis corrupti nobis ad iure illo voluptates, impedit placeat repudiandae culpa cumque! Tempore quis adipisci doloremque expedita. Aspernatur maiores dolorum ipsam neque. Obcaecati tempore et velit modi.
+            </p>
+            <ul>
+               <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam eos similique in accusamus illum distinctio ratione quod? Incidunt enim ipsa perspiciatis magni sequi minima fugit, vel eos, blanditiis autem veritatis!</li>
+               <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur, beatae tempora, eos, quas temporibus quam molestiae vero perferendis voluptatibus fugit a repudiandae hic nemo numquam. Suscipit pariatur non obcaecati distinctio!Totam enim accusantium, similique id dolor excepturi veniam natus doloremque tempora cumque adipisci numquam, dolorem minus rerum magnam obcaecati ducimus quibusdam ut sit nostrum non earum provident sunt expedita. Necessitatibus.</li>
+               <li>Dolar  consectetur adipisicing elit. Consequuntur, beatae tempora,</li>
+               <li> consectetur adipisicing elit. Consequuntur, beatae tempora, consectetur adipisicing elit. Consequuntur, beatae tempora, consectetur adipisicing elit. Consequuntur, beatae tempora,</li>
+            </ul>
+            <ul>
+               <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam eos similique in accusamus illum distinctio ratione quod? Incidunt enim ipsa perspiciatis magni sequi minima fugit, vel eos, blanditiis autem veritatis!</li>
+               <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur, beatae tempora, eos, quas temporibus quam molestiae vero perferendis voluptatibus fugit a repudiandae hic nemo numquam. Suscipit pariatur non obcaecati distinctio!Totam enim accusantium, similique id dolor excepturi veniam natus doloremque tempora cumque adipisci numquam, dolorem minus rerum magnam obcaecati ducimus quibusdam ut sit nostrum non earum provident sunt expedita. Necessitatibus.</li>
+               <li>Dolar  consectetur adipisicing elit. Consequuntur, beatae tempora,</li>
+               <li> consectetur adipisicing elit. Consequuntur, beatae tempora, consectetur adipisicing elit. Consequuntur, beatae tempora, consectetur adipisicing elit. Consequuntur, beatae tempora,</li>
+            </ul>
+            <p className={`${styles.Terms_Topic_Title}`}>
+                Example    
+            </p>
+            <p className={`${styles.Terms_Topic_Blurb}`}>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam saepe quibusdam facilis? Accusamus excepturi, voluptates, placeat magnam quod voluptas esse porro ex vitae repellat at, doloribus blanditiis iste ab. Doloremque?
+                Pariatur, nisi omnis facere, officia porro voluptatibus magnam placeat exercitationem consequuntur repudiandae dolorem dolor illum, quibusdam asperiores commodi. Eaque iure est praesentium adipisci suscipit, laboriosam dolor et excepturi nulla modi.
+                Dolores, rem vitae voluptas voluptatum voluptatibus saepe sed fuga eius ab asperiores, temporibus, tempore minima eligendi dignissimos dicta. Porro, consequatur eligendi. Provident deleniti officia expedita ducimus aliquid omnis sed accusantium!
+                Numquam, possimus qui officiis corrupti nobis ad iure illo voluptates, impedit placeat repudiandae culpa cumque! Tempore quis adipisci doloremque expedita. Aspernatur maiores dolorum ipsam neque. Obcaecati tempore et velit modi.
+            </p>
+        </div>
+        <div className={`${styles.Terms_Area_C}`}>
+            C
+        </div>
+        <div className={`${styles.Terms_Area_D} ${styles.Terms_Update}`}>
+            <p>Updated on 0/0/0</p>
+        </div>
+    </Fragment>
+)
+
+export default terms;
