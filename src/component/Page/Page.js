6c1a642b66765b11f61f72ca0f1619deed8f0990
Billie Tran <0billtran@gmail.com>
2019-10-16 23:52:39 -0700

    Imported more pages, altered footer address, added purchase route

--- src/component/Page/Page.js
+++ src/component/Page/Page.js
@@ -1,8 +1,12 @@
 import React, {Fragment} from 'react';
 import {Switch, Route, Redirect} from 'react-router-dom';
 import {connect} from 'react-redux';
+// LayOut
 import AuthorizedNavBar from '../AuthorizedNavBar/AuthorizedNavBar';
 import Toolbar from '../Header/Toolbar/Toolbar';
+import Footer from '../Footer/Footer';
+// Pages
+import Purchase from '../../container/Purchase/Purchase';
 import LaptopForm from '../../container/Form/LaptopForm';
 import Store from '../Store/Store';
 import Laptop from '../../container/Laptop/Laptop';
@@ -10,7 +14,12 @@ import EditLaptop from '../../container/Form/EditLaptop';
 import Contact from '../../container/Contact/Contact';
 import SignIn from '../SignIn/SignIn';
 import SearchResults from '../../container/SearchResults/SearchResults';
-import Footer from '../Footer/Footer';
+import AboutUs from '../AboutUs/AboutUs';
+import PrivacyPolicy from '../PrivacyPolicy/PrivacyPolicy';
+import Terms from '../Terms/Terms';
+import Brands from '../../container/Brand/Brand';
+import BrandsPage from '../../container/Brand/BrandsPage';
+
 
 const page = (props) => {
     const {authenticated} = props;
@@ -21,11 +30,17 @@ const page = (props) => {
             {authVerified}
             <Switch>
                 <Route exact path="/" component={Store}/>
+                <Route path='/purchase' component={Purchase}/>
                 <Route path="/add" component={LaptopForm}/>
+                <Route path='/laptops/brand/:brand' component={Brands}/>
                 <Route path="/contact" component={Contact}/>
+                <Route path='/laptops/brand' exact component={BrandsPage}/>
                 <Route path="/laptops/:id" exact component={Laptop}/>
                 <Route path='/laptops/edit/:id' component={EditLaptop}/>
                 <Route path='/signin' component={SignIn}/>
+                <Route path='/privacy' component={PrivacyPolicy}/>
+                <Route path="/terms-conditions" component={Terms}/>
+                <Route path='/aboutus' component={AboutUs}/>
                 <Route path='/search' render={(props) => 
                     <SearchResults {...props} />
                 }/>
