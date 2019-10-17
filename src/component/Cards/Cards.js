f534ae79cac6e17dccefedfae67b2f1d48f78536
Billie Tran <0billtran@gmail.com>
2019-10-16 23:48:10 -0700

    Added more styling and divs

--- src/component/Cards/Cards.js
+++ src/component/Cards/Cards.js
@@ -4,36 +4,35 @@ import styles from './Cards.module.css'
 
 
 const cards = (props) => (
-    <div className={styles.Card}>
+    <div className={`${styles.Card_Border_Right} ${styles.Card_Margins}`}>
         <Link to={"/laptops/" + props.id} className={styles.Card}>
+            <p className={styles.Header}>
+                    {props.brand}
+            </p>
             <div className={styles.Image}>
                 <img className={styles.CardImage} src={props.image} alt="test image"/>
             </div>
-            <div>
-                <p className={styles.Header}>
-                    {props.brand}
-                </p>
-                <p>
-                    {props.name}
-                </p>
-                <p>
-                    {props.original}
-                </p>
-                <p>
-                    {props.savings}
-                </p>
-                <p>
-                    {props.price}
-                </p>
-                
-                <p className={styles.Price}>
-                    {props.price}
-                </p>
+            <p className={styles.Name}>
+                {props.name}
+            </p>
+            <div className={styles.Card_Flex_Container}>
+                <div className={styles.Infobox}>
+                    <p className={styles.Original}>
+                        ${props.original}
+                    </p>
+                    <p className={styles.Price}>
+                        ${props.price}
+                    </p>
+                </div>
+                <div className={styles.Card_Infobox_Button_Container}>
+                    <Link to={"/purchase"}>
+                        <button className={styles.Card_Infobox_Button}>
+                            Purchase
+                        </button>
+                    </Link>
+                </div>
             </div>
         </Link>        
-        <button className={styles.Cart}>
-                Add to Cart
-        </button>
     </div>
 );
 
