b352a813b3530783fb99a12317a61a08d8b68d30
Billie Tran <0billtran@gmail.com>
2019-10-24 03:30:51 -0700

    Added a thank you page for purchase redirect

---
+++ src/component/PurchaseThankYou/PurchaseThankYou.js
@@ -0,0 +1,24 @@
+import React, {Fragment} from 'react';
+import styles from './PurchaseThankYou.module.css';
+
+const purchaseThankYou = () => (
+    <Fragment>
+        <div className={styles.PurchaseThankYou_Area_E}>
+        
+        </div>
+        <div className={styles.PurchaseThankYou_Area_A}>
+        
+        </div>
+        <div className={styles.PurchaseThankYou_Area_B}>
+        
+        </div>
+        <div className={styles.PurchaseThankYou_Area_C}>
+        
+        </div>
+        <div className={styles.PurchaseThankYou_Area_D}>
+        
+        </div>
+    </Fragment>
+)
+
+export default purchaseThankYou;
