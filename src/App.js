f14712e96bc4b374c2bbe512fe4769fc06b00ea7
Billie Tran <0billtran@gmail.com>
2019-10-24 02:34:24 -0700

    Added a few more icons to fontawesome

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, {Component} from 'react';
 import {library} from '@fortawesome/fontawesome-svg-core';
-import {faShoppingCart, faSearch, faEnvelope, faPlus, faMinus} from '@fortawesome/free-solid-svg-icons';
+import {faShoppingCart, faSearch, faEnvelope, faPlus, faMinus, faStar, faShippingFast, faSmile} from '@fortawesome/free-solid-svg-icons';
 import Page from './component/Page/Page';
 import styles from './App.module.css';
 
-library.add(faShoppingCart, faSearch, faEnvelope, faPlus, faMinus);
+library.add(faShoppingCart, faSearch, faEnvelope, faPlus, faMinus, faStar,  faShippingFast, faSmile);
 
 class App extends Component{
   render(){
