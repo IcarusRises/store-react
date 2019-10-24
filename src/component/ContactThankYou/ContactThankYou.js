2885d3a9bc1458bd7ad29e116d33e744a801fb9c
Billie Tran <0billtran@gmail.com>
2019-10-24 03:29:17 -0700

    Added a thank you page for contact redirect

---
+++ src/component/ContactThankYou/ContactThankYou.js
@@ -0,0 +1,24 @@
+import React, {Fragment} from 'react';
+import styles from './ContactThankYou.module.css';
+
+const contactThankYou = () => (
+    <Fragment>
+        <div className={styles.ContactThankYou_Area_E}>
+        
+        </div>
+        <div className={styles.ContactThankYou_Area_A}>
+        
+        </div>
+        <div className={styles.ContactThankYou_Area_B}>
+        
+        </div>
+        <div className={styles.ContactThankYou_Area_C}>
+        
+        </div>
+        <div className={styles.ContactThankYou_Area_D}>
+        
+        </div>
+</Fragment>
+)
+
+export default contactThankYou;
